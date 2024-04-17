b4610d3b90aa0cec40941474a9187a62393821f3
informant <artemiy.solodkin@mail.ru>
2024-04-17 17:07:03 +0500

    Work start

---
+++ JavaLaba9/src/main/java/org/example/service/ProductService.java
@@ -0,0 +1,8 @@
+package org.example.service;
+
+import org.example.model.Product;
+
+public class ProductService {
+    private final ArrayList<Product> products = new ArrayList<Product>();
+    
+}
